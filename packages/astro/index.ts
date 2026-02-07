bb56ed4b288f0368191d59e3d07a0fb52a7e31c1
José Carlos Fatjó García <jcfatjo@jctechstudio.com>
2026-02-08 00:28:16 +0100

    feat(astro): add script to generate index file programatically

--- packages/astro/index.ts
+++
@@ -1,6 +0,0 @@
-// Do not write code directly here, instead use the `src` folder!
-// Then, use this file to export everything you want your user to access.
-
-import "./dist/styles.css";
-
-export { default as Button } from "./dist/components/Button/Button.astro";
