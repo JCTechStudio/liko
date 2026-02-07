92968e2069a649b060c9afae6dff0f9e925fc29d
José Carlos Fatjó García <jcfatjo@jctechstudio.com>
2026-02-07 23:47:32 +0100

    fix(astro): fix problem with exported component

--- packages/astro/index.ts
+++ packages/astro/index.ts
@@ -1,4 +1,6 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-export { default as Button } from "./src/components/Button/Button.astro";
+import "./dist/styles.css";
+
+export { default as Button } from "./dist/components/Button/Button.astro";
