45885ecbb778aa2ad91e4f793df9bbbb1c2bf5e9
José Carlos Fatjó García <jcfatjo@jctechstudio.com>
2026-02-08 12:55:49 +0100

    feat(docs): change border radius of code tab to match the preview

--- apps/docs/ec.config.mjs
+++ apps/docs/ec.config.mjs
@@ -1,6 +1,10 @@
+import { defineEcConfig } from '@astrojs/starlight/expressive-code';
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 /** @type {import("@astrojs/starlight/expressive-code").StarlightExpressiveCodeOptions} */
-export default {
+export default defineEcConfig({
   plugins: [pluginLineNumbers()],
-};
+  styleOverrides: {
+    borderRadius: "0.375rem"
+  }
+});
